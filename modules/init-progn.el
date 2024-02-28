cb778a954f19b86df1ef97ec12e612c7bbe86423
qiansheng91 <xin.zhang@tutamail.com>
2024-02-28 22:55:09 +0800

    add dape key bindings

--- modules/init-progn.el
+++ modules/init-progn.el
@@ -60,11 +60,39 @@
 ;; key binding
 (use-package emacs
 	:general
-	(my-leader-def 'normal
+
+  ;; for code
+	(my-leader-def 'normal prog-mode-map
 		"c" '(:ignore t :which-key "code")
 		"ca" '(eglot-code-actions :which-key "action")
 		"cd" '(xref-find-definitions :which-key "definition")
 		"cr" '(xref-find-references :which-key "references")
-		"ci" '(eglot-find-implementation :which-key "implementations")))
+		"ci" '(eglot-find-implementation :which-key "implementations"))
+
+	;; for debug
+	(my-leader-def 'normal prog-mode-map
+	  "d" '(:ignore t :which-key "debug")
+	  "dd" '(dape :which-key "debug")
+	  "db" '(:ignore t :which-key "breakpoint")
+	  "dbt" '(dape-toggle-breakpoint :which-key "toggle")
+	  "dbd" '(dape-delete-breakpoint :which-key "delete")
+	  "dbn" '(dape-next-breakpoint :which-key "next")
+	  "dbp" '(dape-previous-breakpoint :which-key "previous")
+	  "dbf" '(dape-first-breakpoint :which-key "first")
+	  "dbl" '(dape-last-breakpoint :which-key "last")
+	  "de" '(:ignore t :which-key "exception")
+	  "det" '(dape-toggle-exception :which-key "toggle")
+	  "del" '(dape-list-exceptions :which-key "list")
+	  "den" '(dape-next-exception :which-key "next")
+	  "dep" '(dape-previous-exception :which-key "previous")
+	  "def" '(dape-first-exception :which-key "first")
+	  "del" '(dape-last-exception :which-key "last")
+	  "dr" '(:ignore t :which-key "repl")
+	  "dre" '(dape-repl :which-key "start")
+	  "drk" '(dape-kill-repl :which-key "kill")
+	  "drl" '(dape-repl-send-line :which-key "send line")
+	  "drb" '(dape-repl-send-buffer :which-key "send buffer")
+	  ))
+
 
 (provide 'init-progn)
